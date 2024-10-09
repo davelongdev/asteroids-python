f0fd5c686986fb8b57145e796c875ae40c0038e3
dave long <davelongdev@gmail.com>
2024-10-09 11:53:53 -0400

    adjust fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     #game loop
     while True:
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
